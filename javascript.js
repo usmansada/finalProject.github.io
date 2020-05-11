62ce9a7ea8b9d2b0e30c3e6d147a50254ee704f0
usman muhammad sada <usmansada60@gmail.com>
2020-05-11 09:14:24 -1200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,16 @@
 
-
-
-
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
     
  }
 
- 
+
+mybutton = document.getElementById("myBtn");
+
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
